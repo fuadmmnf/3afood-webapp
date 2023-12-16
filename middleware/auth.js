b3038817e53ae19c0c1653430e31849d4504b1ec
minionsrahat <rahatuddin786@gmail.com>
2023-12-17 00:58:33 +0600

    faq added to the public path

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default async function ( {store,route , redirect}) {
     const requiresAuth =  store.getters.isAuthenticated
-    const allowedPaths = ['/','/our-products', '/login','/forgot-password' ,'/register/wholesale', '/register/retail','/about','/services','privacy-policy','/terms-conditions','/online-order','/contact']; // Add allowed paths here
+    const allowedPaths = ['/','/our-products', '/login','/forgot-password' ,'/register/wholesale', '/register/retail','/about','/services','/privacy-policy','/faqs','/terms-conditions','/online-order','/contact']; // Add allowed paths here
     const isAllowedPath = allowedPaths.includes(route.path);
     // console.log('Middleware running...');
     // console.log('Required Auth:',requiresAuth)
